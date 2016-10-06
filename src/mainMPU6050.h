3d315ae13c004316bb514ef2173912fb7f659023
createproblem <createproblem@hushmail.com>
2016-10-06 09:43:44 +0200

    stuff

--- src/mainMPU6050.h
+++ src/mainMPU6050.h
@@ -4,12 +4,14 @@
 MPU6050 mpu6050(MPU6050_DEFAULT_ADDRESS);
 
 void setup()
-{}
+{
+  printf("MPU6050 online: %s\n", mpu6050.isConnected() ? "YES" : "NO");
+}
 
 void loop()
 {
-  printf("MPU6050 online: %s\n", mpu6050.isConnected() ? "YES" : "NO");
+  printf("========= CONFIG =========\n");
   printf("DeviceId: 0x%0x\n", mpu6050.getDeviceId());
-
+  printf("\n");
   delay(3000);
 }
